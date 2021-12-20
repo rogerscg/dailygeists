47c5cf0c2dd539457451b4ad358f2d6c89cb35d1
Greg Rogers <rogerscgreg@gmail.com>
2021-12-19 23:31:38 -0500

    Updated to use keyboard instead of pynput

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
-from pynput.keyboard import Key, Listener
+import keyboard
 from gpiozero import LED
 
 led = LED(17)
 LED_KEY = "1"
+ESC = "esc"
+enabled = True
+
+key_state = {
+    LED_KEY: False,
+}
 
 
-def on_press(key):
-    if key != LED_KEY:
-        return True
-    print('{0} pressed'.format(key))
-    led.on()
+def handle_key_presses():
+    global enabled
+    if keyboard.is_pressed(ESC):
+        enabled = False
+        return
+    for key in key_state:
+        key_state[key] = keyboard.is_pressed(key)
 
 
-def on_release(key):
-    if key == Key.esc:
-        # Stop listener
+def handle_key_states():
+    if key_state[LED_KEY]:
+        led.on()
+    else:
         led.off()
-        return False
-    if key != LED_KEY:
-        return True
-    print('{0} release'.format(key))
-    led.off()
 
 
 def main():
-    # Collect events until released
-    with Listener(
-            on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()
+    global enabled
+    while enabled:
+        handle_key_presses()
+        handle_key_states()
+        continue
 
 
 if __name__ == "__main__":
